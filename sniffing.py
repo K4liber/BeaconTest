a2e628c8a9f8d36c26df967e778492361ee7c840
K4liber <janbielecki94@gmail.com>
2019-01-05 19:52:02 +0100

    Data

--- sniffing.py
+++ sniffing.py
@@ -14,16 +14,18 @@ sudo airodump-ng --beacons wlp2s0
 '''
 
 ap_list = []
+f= open("6metr.txt","w+")
 
 def PacketHandler(pkt) :
     if pkt.haslayer(Dot11) :
         if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
-            try:
-                extra = pkt.notdecoded
-                rssi = -(256-ord(extra[-4:-3]))
-            except:
-                rssi = -100
-            print "WiFi signal strength:", rssi, "dBm of", pkt.addr2, pkt.info
+			try:
+				extra = pkt.notdecoded
+				rssi = -(256-ord(extra[-2:-1]))
+			except:
+				rssi = -100
+			print "WiFi signal strength:", rssi, "dBm of", pkt.addr2, pkt.info
+			f.write("Addr: " + pkt.addr2 + " Name: " + pkt.info + " Strength: " + str(rssi))
 
-
-sniff(iface="wlp2s0", prn = PacketHandler)
+sniff(iface="mon0", prn = PacketHandler)
+f.close() 
